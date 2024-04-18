43049d019865817350ab8674ced366fdc9ad25f4
Michel Oliveira e Oliveira <michel.oliveira@digitra.com>
2024-04-18 18:54:09 -0300

    Update docs.routeBasePath to /

--- assets/js/c4f5d8e4.6969f666.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktest_website=self.webpackChunktest_website||[]).push([[195],{62841:(e,t,s)=>{s.r(t),s.d(t,{default:()=>r});var c=s(67294),n=s(16550);const r=function(){return c.createElement(n.l_,{to:"/docs/docs/Overview"})}}}]);
